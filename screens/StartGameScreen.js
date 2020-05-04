996a583dc80017b8723fea07f092cdc455de7583
Seth Wheeler <wheeler.seth@gmail.com>
2020-05-04 10:15:47 -0600

    added styling structure for Input section

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
+import Card from '../components/Card'
+import Colors from '../constants/colors'
+
 const StartGameScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game!</Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
-          <Button title="Reset" onPress={() => {}} />
-          <Button title="Confirm" onPress={() => {}} />
+          <View style={styles.button}><Button title="Reset" onPress={() => {}} color={Colors.accent}/></View> 
+          <View style={styles.button}><Button title="Confirm" onPress={() => {}} color={Colors.primary}/></View> 
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
@@ -26,7 +29,7 @@ const styles = StyleSheet.create({
   inputContainer: {
     width: 300,
     maxWidth: "80%",
-    alignItems: 'center'
+    alignItems: 'center',  
   },
   buttonContainer: {
     flexDirection: "row",
@@ -38,6 +41,9 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginVertical: 10,
   },
+  button:{
+    width: 100,
+  }
 });
 
 export default StartGameScreen;
