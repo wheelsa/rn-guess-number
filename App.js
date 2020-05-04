b60de1e84c390191e59796eb00fe782b7186b115
Seth Wheeler <wheeler.seth@gmail.com>
2020-05-04 12:19:53 -0600

    established computer guess logic, and created game over screen

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Header from './components/Header';
-import StartGameScreen from './screens/StartGameScreen';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+
+import Header from "./components/Header";
+import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
+import GameOverScreen from "./screens/GameOver";
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
+
+  const handleStartGame = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+    setGuessRounds(0);
+  };
+
+  const gameOverHandler = (numOfRounds) => {
+    setGuessRounds(numOfRounds);
+  };
+
+  let content = <StartGameScreen onStartGame={handleStartGame} />;
+
+  if (userNumber && guessRounds <= 0) {
+    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+  } else if (guessRounds > 0) {
+    content = <GameOverScreen />;
+  }
+
   return (
-    <View style={styles.screen} >
+    <View style={styles.screen}>
       <Header title="Guess a Number" />
-      <StartGameScreen />
+      {content}
     </View>
-  ); 
+  );
 }
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-  }
+  },
 });
 //occup full screen as root element
---
+++ screens/GameScreen.js
@@ -0,0 +1,90 @@
+import React, { useState, useRef, useEffect } from "react";
+import { View, Text, StyleSheet, Button, Alert } from "react-native";
+
+import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+};
+
+const GameScreen = (props) => {
+
+  const {userChoice, onGameOver} = props;
+
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, userChoice)
+  );
+
+  const [rounds, setRounds] = useState(0);
+
+  const currentLow = useRef(1); //component will not rerender when number changes
+  const currentHigh = useRef(100);
+  
+
+  useEffect(() => {
+    if (currentGuess === userChoice) {
+     onGameOver(rounds);
+    }
+  }, [currentGuess, onGameOver, userChoice]); //only if these three values change
+
+  const handleNextGuess = (direction) => {
+    if (
+      (direction === "lower" && currentGuess < userChoice) ||
+      (direction === "greater" && currentGuess > userChoice)
+    ) {
+      Alert.alert("Don't lie!", "You know that this is wrong...", [
+        { text: "Sorry!", style: "cancel" },
+      ]);
+      return;
+    }
+    if (direction === "lower") {
+      currentHigh.current = currentGuess;
+    } else {
+      currentLow.current = currentGuess;
+    }
+    const nextNumber = generateRandomBetween(
+      currentLow.current,
+      currentHigh.current,
+      currentGuess
+    );
+    setCurrentGuess(nextNumber);
+    setRounds((curRounds) => curRounds + 1);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title="LOWER" onPress={handleNextGuess.bind(this, "lower")} />
+        <Button title="HIGHER" onPress={handleNextGuess.bind(this, "higher")} />
+      </Card>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 400,
+    maxWidth: "80%",
+  },
+});
+
+export default GameScreen;
